ed78569f17d94eba95cb489657611b63302ecad6
Matias Alfaro <malfarodev@gmail.com>
2024-05-31 00:51:31 -0300

    Release 1.0 Bugfixes

--- gameEngine.cpp
+++ gameEngine.cpp
@@ -1,13 +1,19 @@
 #include <iostream>
 #include <time.h>
-#include <vector>
+#include <vector> /* REASONS OF USING:
+
++ AUTOMATIC RESIZE Without the need of pointers, no hardcoded size.
++ board.size() makes everything dynamic, allowing custom settings. 
++ Implementations have been taught thinking about future enhancements */
 
 #define EMPTY_CELL 0
-#define BOMB -1
+
+#define BOMB -1 // Use of negative numbers were implemented in order to prevent mix ups.
 #define FLAG -2
+#define TRUEFLAG -3 // A FLAG is true when its above a BOMB.
 #define REVEALED -8
 
-// ANSI color codes
+// ANSI color codes 
 #define BLUE "\033[36m"
 #define DARK_BLUE "\033[34m"
 #define GREEN "\033[32m"
@@ -19,7 +25,9 @@
 #define WHITE  "\033[37m"
 #define GREY "\033[90m"
 
-// Function to get color code for a number
+int moves, flags; // Global moves & flags counters
+
+// Function to get color code for a number (Loyal to OG MineSweeper). 
 std::string getColor(int num) {
 
     switch (num) {
@@ -36,97 +44,131 @@ std::string getColor(int num) {
     }
 }
 
-// Dynamic Board Generation (By Difficulty)
+// Dynamic Board Generation (By Difficulty).
 std::vector<std::vector<int>> dynamicBoard(int ROWSIZE, int COLSIZE) {
 
-    std::vector<std::vector<int>> board(ROWSIZE + 1, std::vector<int>(COLSIZE + 1, EMPTY_CELL));
+    // Create VECTOR by ROWSIZE & COLSIZE.
+    std::vector<std::vector<int>> board(ROWSIZE, std::vector<int>(COLSIZE, EMPTY_CELL));
     return board;
 }
 
-// Dynamic bomb generation. Number of Bombs / Empty Board (dynamicBoard)
+// Dynamic bomb generation. Number of Bombs / Empty Board (dynamicBoard).
 void randBombGen(int bombNum, std::vector<std::vector<int>>& board) {
 
     int bombCount = 0; // Bomb COUNT limiter.
     int rowCount = board.size(), colCount = board[0].size();
 
     while (bombCount < bombNum) {
-        int row = rand() % rowCount, col = rand() % colCount; // Takes a Random row/col coordinate
+
+        int row = rand() % rowCount, col = rand() % colCount; // Takes a Random row/col coordinate.
+
         if (board[row][col] != BOMB) {
-            board[row][col] = BOMB;
-            bombCount++;
-        }
+
+                board[row][col] = BOMB;
+                bombCount++;
+            }
     }
 }
 
 // Number of bombs in adjacent cells of user selected coordinates.
 int bombNearNum(int x, int y, const std::vector<std::vector<int>>& board) {
 
-    int bombNear = 0; // Initialize bombNear
-        
+    int bombNear = 0; // Initialize bombNear.
+
+    // FOR loop that iterates through the board.
     for (int i = x - 1; i <= x + 1; i++) {
-        for (int j = y - 1; j <= y + 1; j++) { // NEEDS UPDATING.
+        for (int j = y - 1; j <= y + 1; j++) { 
+
             if (i >= 0 && i < board.size() && j >= 0 && j < board[0].size() && board[i][j] == BOMB) {
                 bombNear++;
             }
         }
     }
 
-    return bombNear; // Return the count of nearby bombs
+    return bombNear; // Return the count of nearby bombs.
 }
 
+// BOOL -> FALSE: Recursive Function | TRUE: Bomb has been hit.
 bool exploreBoard(int x, int y, std::vector<std::vector<int>>& board) {
-    // Check if the current cell is out of bounds or already revealed
-    if (x < 0 || x >= board.size() || y < 0 || y >= board[0].size() || board[x][y] == REVEALED || board[x][y] == FLAG) 
-        return false;
+   
+    // Check if the current cell is out of bounds or already revealed.
+    if (x < 0 || x >= board.size()|| 
+        y < 0 || y >= board[0].size()|| 
+        board[x][y] == REVEALED ||
+        board[x][y] == FLAG || 
+        board[x][y] == TRUEFLAG ) 
+            return false;
 
-    // If the current cell is a bomb, return true indicating game over
+    // If the current cell is a BOMB, return TRUE indicating game over.
     if (board[x][y] == BOMB) 
         return true;
 
-    // Calculate the number of adjacent bombs
+    // Calculate the number of adjacent BOMB.
     int adjacentBombs = bombNearNum(x, y, board);
-
-    // If there are adjacent bombs, reveal the current cell and return
-    if (adjacentBombs > 0) {
+    if (adjacentBombs > 0) { // If there are adjacent BOMB, reveal the current cell and return.
         board[x][y] = adjacentBombs;
         return false;
     }
 
-    // If there are no adjacent bombs, reveal the current cell and recursively explore adjacent cells
+    // If there are no adjacent BOMB, reveal the current cell and recursively explore adjacent cells.
     board[x][y] = REVEALED;
     for (int i = x - 1; i <= x + 1; i++) {
         for (int j = y - 1; j <= y + 1; j++) {
-            if (i != x || j != y) { // Avoid infinite loop by not revisiting the same cell
+            if (i != x || j != y) { // Avoid infinite loop by not revisiting the same cell.
                 exploreBoard(i, j, board);
             }
         }
     }
+
     return false;
 }
-
 
 // Console print the board by values (EMPTY_CELL, BOMB, FLAG, REVEALED)
 void boardPrint(const std::vector<std::vector<int>>& board, bool revealBombs = false) {
     
+        // FOR loop that iterates through the board.
         for (int i = 0; i < board.size(); i++) {
         for (int j = 0; j < board[0].size(); j++) {
 
-            if (revealBombs && board[i][j] == BOMB) {
-                std::cout << MAGENTA << "* " << WHITE;
+            if (revealBombs && board[i][j] == BOMB) { std::cout << MAGENTA << "* " << WHITE; 
 
-            } else if (board[i][j] == EMPTY_CELL || board[i][j] == BOMB) {
-                std::cout << "- ";
+            } else if (board[i][j] == EMPTY_CELL || board[i][j] == BOMB) { std::cout << "- ";
 
-            } else if (board[i][j] == REVEALED) {
-                std::cout << GREY <<  ". " << WHITE;
+            } else if (board[i][j] == REVEALED) { std::cout << GREY <<  ". " << WHITE;
 
-            } else if (board[i][j] == FLAG) {
-                std::cout << "⚑ ";
+            } else if ((board[i][j] == FLAG) || (board[i][j] == TRUEFLAG)) { std::cout << "⚑ ";
 
-            } else {
-                std::cout << getColor(board[i][j]) << board[i][j] << WHITE << " "; // Display number of bombs in adjacent cells with color
-            }
+            } else { std::cout << getColor(board[i][j]) << board[i][j] << WHITE << " "; } // Display number of bombs in adjacent cells with color
         }
+
         std::cout << std::endl;
     }
+}
+
+// Game Win indicator
+bool gameWin(const std::vector<std::vector<int>>& board) {
+
+    int boardSize = board.size() * board[0].size(); // Total ammount of cells.
+    int revealedCells = 0, flaggedCells = 0;
+
+    // FOR loop that iterates through the board.
+    for (int i = 0; i < board.size(); ++i) {
+        for (int j = 0; j < board[0].size(); ++j) {
+
+            // Counts REVEALED.
+            if (board[i][j] == REVEALED) revealedCells++;
+
+            // Counts BOMB
+            if (board[i][j] >= 0 && board[i][j] <= 8) revealedCells++;
+
+            // Counts TRUEFLAG.E
+            if (board[i][j] == TRUEFLAG) flaggedCells++;
+
+            // Check incorrectly flagged cells (FLAG)
+            if (board[i][j] == FLAG && board[i][j] != BOMB) return false; 
+        }
+    }
+
+    // WIN: TRUE -> All REVEALED & All TRUEFLAG 
+    return revealedCells + flaggedCells == boardSize;
 }
--- main.cpp
+++ main.cpp
@@ -3,35 +3,35 @@
 
 int main() {
     
-    srand(time(0)); // Seed for random number generation
+    srand(time(0)); // Seed for random number generation.
 
-    int boardSizeX, boardSizeY, bombsCount;
-    char difficulty;
+    int sizeY, sizeX, bombsCount;
+    char difficulty, understood;
    
     startingGUI();
-        std::cin >> difficulty; // Dynamic Difficulty, you can change up board X & Y size below
+        std::cin >> difficulty; // Dynamic Difficulty, you can change up board X & Y size below.
 
         while (true) {
 
-                if (difficulty == 'A') { boardSizeX = boardSizeY = 8, bombsCount = 16; break; }
-                    else if  (difficulty == 'B') { boardSizeX = boardSizeY = 16, bombsCount = 40; break; }
-                        else if  (difficulty == 'C') { boardSizeX = 10, boardSizeY = 45, bombsCount = 50; break; }
+                if (difficulty == 'A') { sizeY = sizeX = 8, bombsCount = 16; break; } // Beginner.
+                    else if  (difficulty == 'B') { sizeY = sizeX = 16, bombsCount = 40; break; } // Intermediate.
+                        else if  (difficulty == 'C') { sizeY = 10, sizeX = 45, bombsCount = 50; break; } // Advanced.
                             else { std::cout << MAGENTA  << "  ____________________________________________________" << std::endl << "  -Miney: "<< WHITE <<"Don't be a meanie, select a valid DIFFICULTY: "; 
                                 std::cin >> difficulty; }
         };
 
     system("clear");
 
-    char understood;
         while (true) {
 
             rules(bombsCount);
+
             std::cout << MAGENTA  << "  ________________________" << std::endl << "  -Miney: "<< WHITE <<"Understood? [Y/N]: " << WHITE;
             std::cin >> understood;
 
-            if (understood == 'Y') {break; system("clear");}
-            else {
-
+            if (understood == 'Y') { break; system("clear"); } 
+            else { 
+                
                 system("clear");
                 std::cout << MAGENTA  << "  ______________________" << std::endl << "  -Miney: "<< WHITE <<"Read 'em again." << WHITE << std::endl;
             }
@@ -39,17 +39,18 @@ int main() {
     
     system("clear");
     
-    std::vector<std::vector<int>> gameBoard = dynamicBoard(boardSizeX, boardSizeY);
+    // Generate board with sizeY & sizeX defined in "difficulty".
+    std::vector<std::vector<int>> gameBoard = dynamicBoard(sizeY, sizeX);
     randBombGen(bombsCount, gameBoard);
 
     
-    // Game loop, returns false if a bomb gets hit.
+    // Game loop, exits when FALSE (BOMB Hit).
     while (true) {
 
         boardPrint(gameBoard);
 
         // User input.
-        int x, y, moves, flags;
+        int x, y, safeX, safeY;
         char action;
 
         std::cout << MAGENTA  << std::endl << MAGENTA  << " MOVES: " << WHITE << moves << MAGENTA << "                              FLAGS: " << WHITE << flags << MAGENTA << "/" <<  WHITE << bombsCount << std::endl;
@@ -58,8 +59,16 @@ int main() {
         std::cin >> x >> y >> action;
         system("clear");
 
+        if (gameWin(gameBoard)) {
+
+            boardPrint(gameBoard);
+            std::cout << MAGENTA << " _______________________________" << std::endl << " -Miney: " << WHITE << "Congratulations! You win!" << WHITE << std::endl;
+            break;
+
+        }
+
         // Validate user COORDINATES input.
-        if (x < 0 || x >= boardSizeX || y < 0 || y >= boardSizeY) {
+        if (x < 0 || x >= sizeY || y < 0 || y >= sizeX) {
 
             std::cout << MAGENTA << " ______________________________________" << std::endl << " -Miney: " << WHITE << "Please, enter valid COORDINATES." << WHITE << std::endl << "      " << std::endl;
             std::cout << WHITE << "      " << std::endl;
@@ -69,21 +78,20 @@ int main() {
         // Validate user ACTION input.
         if (action != 'E' && action != 'F') {
 
-            
             std::cout << MAGENTA  << " __________________________________" << std::endl << "-Miney: "<< WHITE <<"Please enter a valid ACTION." << WHITE << std::endl << "      " << std::endl;
             std::cout << WHITE << "      " << std::endl;
             continue;
         }
         
-        // Take action based on user input
+        // Take action based on user input.
         if (action == 'E') {
 
+            moves++; // For each valid move it increases by one.
             if (gameBoard[x][y] == REVEALED) std::cout << MAGENTA  << " __________________________________________________________" << std::endl << " -Miney: "<< WHITE <<"Invalid input. You can't Explore a discovered cell." << WHITE << std::endl << "      " << std::endl;
-            moves++;
-
-            if (exploreBoard(x, y, gameBoard) ) { // (if (TRUE)) Triggers when a bombs is found.
+            
+            if (exploreBoard(x, y, gameBoard) ) { // (if (TRUE) | Triggers when a BOMB is found.
                 
-                // Print the board with bombs revealed.
+                // Print the board with ALL BOMB revealed.
                 boardPrint(gameBoard, true);
                 std::cout << MAGENTA  << " _________________________________" << std::endl << " -Miney: " << WHITE << "You hit a bomb. GAME OVER.\n" << WHITE << std::endl;
                 break;
@@ -92,18 +100,57 @@ int main() {
 
         } else if (action == 'F') {
 
-            // Toggle flag on the cell
+            moves++; // For each valid move it increases by one.
+            // Toggle FLAG on the cell.
             switch(gameBoard[x][y]) {
-
+                
+                // Cases when a FLAG is/isn't placed.
                 case REVEALED : std::cout << MAGENTA  << " _______________________________________________________" << std::endl << " -Miney: "<< WHITE <<"Invalid input. You can't flag a discovered cell." << WHITE << std::endl << "      " << std::endl; continue;
                 case FLAG : gameBoard[x][y] = EMPTY_CELL; moves++ ; flags--; continue;
+                case TRUEFLAG : gameBoard[x][y] = EMPTY_CELL; moves++ ; flags--; continue;
                 case EMPTY_CELL : gameBoard[x][y] = FLAG; moves++; flags++; continue;
-                case BOMB : gameBoard[x][y] = FLAG; moves++; flags++; continue;
-                default : std::cerr << RED <<  "[CRITICAL ERROR] Unknow State. Check Code!\n" << WHITE; continue; // It never should reach this.
+                case BOMB : gameBoard[x][y] = TRUEFLAG; moves++; flags++; continue; // FLAG over bombs get a different value. (TRUEFLAG).
+                default : std::cerr << RED <<  "[CRITICAL ERROR]" << WHITE << std::endl; continue; // It never should reach this.
             }   
+        }
+
+        // Check WIN after TRUEFLAG.
+        if (flags == bombsCount) {
+            
+            bool allBombsFlagged = true;
+                // Check BOMB is TRUEFLAG.
+                for (int i = 0; i < sizeX; i++) {
+                    for (int j = 0; j < sizeY; j++) {
+                        
+                        if (gameBoard[i][j] == BOMB && gameBoard[i][j] != TRUEFLAG) {
+                            allBombsFlagged = false;
+                            break;
+                        }
+                    }
+                }
+            
+
+            bool allCellsRevealed = true;
+                // Check REVEALED.
+                for (int i = 0; i < sizeX; i++) {
+                    for (int j = 0; j < sizeY; j++) {
+                        
+                        if (gameBoard[i][j] != REVEALED && gameBoard[i][j] != BOMB) {
+                            allCellsRevealed = false;
+                            break;
+                        }
+                    }
+                }
+
+            if (allBombsFlagged && allCellsRevealed) {
+            
+                boardPrint(gameBoard);
+                std::cout << MAGENTA << " _______________________________" << std::endl << " -Miney: " << WHITE << "Congratulations! You win!" << WHITE << std::endl;
+                return 0; 
+            }
         }
     };
 
-return 0;
+    return 0;
 
 }
